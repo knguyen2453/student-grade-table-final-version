03e781b5f5b07d8b96a47dd2bc01d825c3be7677
knguyen2453 <yeaitskevinn@yahoo.com>
2020-08-25 19:51:02 -0700

    Add a GradeForm Component

---
+++ components/grade-form.js
@@ -0,0 +1,14 @@
+class GradeForm {
+  constructor(formElement) {
+    this.formElement = formElement;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement.onSubmit = this.handleSubmit;
+  }
+  onSubmit(createGrade) {
+    this.createGrade = createGrade;
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("hi");
+  }
+}
